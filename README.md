0025f0b7efc5f220623ac483b2205715d31e3762
IAmOZRules <shreyaansnahata2001@gmail.com>
2022-01-01 13:23:00 +0530

    added season range rename

--- README.md
+++ README.md
@@ -1,35 +1,63 @@
 # MKVTools
+
 Python script to rename episodes and subtitles (names taken from from IMDb) and video tags (changed to video file title for matroska files).
 
-## Usage
+## Usage:
+
 Make sure you have all the requirements installed!
 
-If you dont, simply run ```pip install -r requirements.txt``` in any terminal, and you are good to go!
+If you dont, simply run ``pip install -r requirements.txt`` in any terminal, and you are good to go!
+
+To use the tags renaming function, you need to have ```mkvpropedit``` from [MKVToolNix](https://mkvtoolnix.download/) added to your System PATH.
 
 ```
 usage: python rename.py [-h] [options]
 
 optional arguments:
-  -h, --help        show this help message and exit
-  -c , --code       IMDb CODE for the Series
-  -e , --episode    Rename EPISODES of the series in the specified Directory
-  -s , --subtitle   Rename SUBTITLES of the Series in the Directory
-  -t , --tag        Rename VIDEO TAGS for MKV Videos
+  -h, --help            show this help message and exit
+  -c , --code           IMDb CODE for the Series
+  -r  [ ...], --range  [ ...]
+                        Season range for rename
+  -e , --episode        Rename EPISODES of the series in the specified Directory
+  -s , --subtitle       Rename SUBTITLES of the Series in the Directory
+  -t , --tag            Rename VIDEO TAGS for MKV Videos
 ```
 
-## Example
+## Examples:
+
+* Renaming all episodes in a series:
+
 ```
 python rename.py -c tt000472954 -e "D:\Episode Directory" -s "D:\Subtitle Directory" -t "D:\Episode Directory"
 python rename.py --code tt000472954 --episode "D:\Episode Directory" --subtitle "D:\Subtitle Directory" --tag "D:\Episode Directory"
 ```
 
+* Renaming episodes upto a certain season in a series:
+```
+python rename.py -c tt000472954 -r 0 4 -e "D:\Episode Directory" -s "D:\Subtitle Directory" -t "D:\Episode Directory"
+```
+
+* Renaming episodes from a certain season to the end of a series:
+```
+python rename.py -c tt000472954 -r 4 0 -e "D:\Episode Directory" -s "D:\Subtitle Directory" -t "D:\Episode Directory"
+```
+
+* Renaming one specified season in a series:
+```
+python rename.py -c tt000472954 -r 4 -e "D:\Episode Directory" -s "D:\Subtitle Directory" -t "D:\Episode Directory"
+```
+
+### Example Output:
+![](images/example.jpg)
 ### NOTES:
+
 - Individual scripts can be found [here](https://github.com/IAmOZRules/MKVTools/tree/main/Individual%20Scripts), but won't be receiving updates!
 - Make sure that **ALL** episodes/subtitles are available in the one folder, not nested folders!
 - Due to certain limitations not yet handled by me, ensure that the **Episodes and Subtitles are in separate folders**!
 
 ## TODO:
-- [x] Convert separate scripts into a single CLI operation ✅
-- [ ] Add custom season/episode range to be renamed
+
+- [X] Convert separate scripts into a single CLI operation ✅
+- [X] Add custom season/episode range to be renamed
 - [ ] Renaming in nested directories
 - [ ] Maybe a GUI 🤷‍♂️
--- rename.py
+++ rename.py
@@ -1,11 +1,21 @@
 import os
-import sys
 import imdb
 import argparse
 from termcolor import colored
 
+'''
+Arguments for the script
+
+    -c, --code      : IMDb Code
+    -r, --range     : Season range for rename
+    -e, --episode   : Directory to rename the episodes
+    -s, --subtitle  : Directory to rename the subtitles
+    -t, --tag       : Directory to edit the tags
+'''
+
 arg_parser = argparse.ArgumentParser(usage='python rename.py [-h] [options]')
 arg_parser.add_argument('-c', '--code', type=str, help='IMDb CODE for the Series', required=False, metavar='')
+arg_parser.add_argument('-r', '--range', type=int, nargs="+", help='Season range for rename', required=False, metavar='', default=[0,0])
 arg_parser.add_argument('-e', '--episode', type=str, help='Rename EPISODES of the series in the specified Directory', default=False, metavar='')
 arg_parser.add_argument('-s', '--subtitle', type=str, help='Rename SUBTITLES of the Series in the Directory', default=False, metavar='')
 arg_parser.add_argument('-t', '--tag', type=str, help='Rename VIDEO TAGS for MKV Videos', required=False, metavar='')
@@ -14,19 +24,49 @@ args = arg_parser.parse_args()
 
 not_allowed = r":\*<>\|"
 
-def get_episodes(imdb_code):
+def get_episodes(imdb_code, start=0, end=0):
+    """
+    Function to get Episode List from IMDb using the IMDb Code
+
+    Parameters
+    ----------
+    imdb_code : str
+        IMDb Code for the Series
+    start : int
+        Season range for rename
+    end : int
+        Season range for rename
+
+    Returns
+    -------
+    episode_list : list
+        List of episodes to be renamed
+    """
+
     ia = imdb.IMDb()
     if "tt" in imdb_code: imdb_code = imdb_code[2:]
 
     series = ia.get_movie(imdb_code)
-    print("SERIES: {}".format(colored(series, "magenta")))
+    print("\nSERIES: {}".format(colored(series, "magenta")))
 
     ia.update(series, "episodes")
     episodes = series.data['episodes']
 
     episode_list = []
 
-    for season in episodes.keys():
+    seasons_list = []
+    if start == 0 and end == 0:
+        seasons_list = [i for i in range(1, len(episodes)+1)]
+    elif start != 0 and end == 0:
+        seasons_list = [i for i in range(start, len(episodes)+1)]
+    elif start == 0 and end != 0:
+        seasons_list = [i for i in range(1, end+1)]
+    else:
+        seasons_list = [i for i in range(start, end+1)]
+
+    print("SEASONS: {}\n".format(colored(seasons_list, "magenta")))
+
+    for season in seasons_list:
         for episode in episodes[season]:
             title = episodes[season][episode]['title']
 
@@ -53,6 +93,17 @@ def get_episodes(imdb_code):
     return episode_list
 
 def rename_episode(directory, episode_list):
+    """
+    Function to rename the episodes in the specified directory
+
+    Parameters
+    ----------
+    directory : str
+        Directory to rename the episodes
+    episode_list : list[str]
+        List of episodes to be renamed    
+    """
+
     files = os.listdir(directory)
     print("Renaming episodes...")
 
@@ -69,6 +120,17 @@ def rename_episode(directory, episode_list):
     print(colored("RENAMING", "red"), colored("EPISODES", "cyan"), colored("FINISHED!\n", "red"))
 
 def rename_subtitle(directory, episode_list):
+    """
+    Function to rename the subtitles in the specified directory
+
+    Parameters
+    ----------
+    directory : str
+        Directory to rename the subtitles
+    episode_list : list[str]
+        List of episodes to be renamed
+    """
+
     files = os.listdir(directory)
     print("Renaming subtitles...")
 
@@ -85,23 +147,38 @@ def rename_subtitle(directory, episode_list):
     print(colored("RENAMING", "red"), colored("SUBTITLES", "cyan"), colored("FINISHED!\n", "red"))
 
 def edit_tag(directory):
+    """
+    Function to edit tags of the MKV files to their file name in the specified directory
+
+    Parameters
+    ----------
+    directory : str
+        Directory to edit the tags
+    """
+
     for file in os.listdir(directory):
         if ".mkv" in file:
             title = file.replace(".mkv", "")
-            command = r'mkvpropedit "{}\{}" --set "title={}"'.format(directory, file, title)
-            os.system(command)
         
         if ".mp4" in file:
             title = file.replace(".mp4", "")
-            command = r'mkvpropedit "{}\{}" --set "title={}"'.format(directory, file, title)
-            os.system(command)
+
+        command = r'mkvpropedit "{}\{}" --set "title={}"'.format(directory, file, title)
+        os.system(command)
         
     print(colored("RENAMING", "red"), colored("VIDEO TAGS", "cyan"), colored("FINISHED!\n", "red"))
 
 if __name__ == "__main__":
+    if args.range:
+        if len(args.range) == 2:
+            start, end = args.range[0], args.range[1]
+        else:
+            start = end = args.range[0]
+    else:
+        start, end = 0, 0
 
     if args.code:
-        episodes = get_episodes(args.code)
+        episodes = get_episodes(args.code, start, end)
 
     if args.episode:
         args.episode = r'{}'.format(args.episode)
